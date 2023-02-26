604b9b3072cf973027970067d76487b53e36b4ec
Roberto Menegassi <81974099+RMenegassi@users.noreply.github.com>
2023-02-25 21:05:45 -0300

    create app

---
+++ mobile/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import {StatusBar, Text, View} from 'react-native';
+
+function App(): JSX.Element {
+  return (
+    <View>
+      <StatusBar barStyle="light-content" backgroundColor="white" />
+      <Text>Open up App.tsx</Text>
+    </View>
+  );
+}
+
+export default App;
